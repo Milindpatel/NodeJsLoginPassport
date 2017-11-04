1236681b8a3feba3ea7181fdc21e8f5f7cf3589a
Milindpatel <milindpatel3959@gmail.com>
2017-11-03 22:53:00 -0400

    make login robust

--- routes/users.js
+++ routes/users.js
@@ -59,19 +59,47 @@ router.post('/register', function(req, res){
 
 passport.use(new LocalStrategy(
     function(username, password, done) {
-     User.getUserByUserName(username, function(err, user){
-        if(err) throw err;
-        if(!user){
-            return done(null, false, {})
-        }
+        User.getUserByUserName(username, function(err, user){
+            if(err) throw err;
+            if(!user){
+                return done(null, false, {message: 'Unknown User'});
+            }
+
+        User.comparePassword(password, user.password, function(err, isMatch){
+            if(err) throw err;
+            if(isMatch){
+                return done(null, user);
+            }else{
+                return done(null, false, {message: 'Invalid password'});
+            }
+        })
      })
     }
   ));
 
-app.post('/login',
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.getUserById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+  router.post('/login',
     passport.authenticate('local',{successRedirect: '/', failureRedirect: '/users/login', failureFlash: true}),
     function(req, res) {
         res.redirect('/');
     });
+
+
+    router.get('/logout',function(req,res){
+        req.logOut();
+        req.flash('success_msg', 'You are logged out');
+        res.redirect('/users/login');
+    });
+ 
 
 module.exports = router;
